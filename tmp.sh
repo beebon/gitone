eb30972a64431d0e8cf206796aa8b2f9b50529a6
beebon <yaqi_zhang@163.com>
2013-09-06 14:54:21 +0800

    commitone

--- tmp.sh
+++ tmp.sh
@@ -1,6 +1,11 @@
 cd ~/'Ubuntu One'/shell
-if [ -z "$1" ]; then echo commit remark required;exit; fi
-git add -i #交互模式
+if [ -z "$1" ]
+then echo 'commit remark required';exit
+fi
+if [ -z "$2" ]
+then git add .
+else git add -i #交互模式
+fi
 git commit -am "$1"
 git remote add origin https://github.com/beebon/gitone.git
 git push -u origin master -f #force push
