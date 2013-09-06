ef3c8fd1644015b9c2d99d1098cbf54359483e49
beebon <yaqi_zhang@163.com>
2013-09-06 14:46:17 +0800

    commitone

--- s/u.sh
+++ s/u.sh
@@ -55,15 +55,14 @@ sudo vim $BASHRC
 #gitone
 #if [ -z "$1"] then echo "缺少git项目名称";exit fi
 cd ~/'Ubuntu One'/shell
-if [ -n "$1"]; then git init --bare;git add .; fi
-git add -i #交互模式
+if [ -n "$1" ]; then git init --bare;git add .;else git add -i; fi
 git commit -am 'fisrt commit shell'
 git remote add origin https://github.com/beebon/gitone.git
 git push -u origin master -f #force push
 #gitone
 #commitone
 cd ~/'Ubuntu One'/shell
-if [ -z "$1"]; then echo commit remark required;exit; fi
+if [ -z "$1" ]; then echo commit remark required;exit; fi
 git add -i #交互模式
 git commit -am "$1"
 git remote add origin https://github.com/beebon/gitone.git
--- tmp.sh
+++ tmp.sh
@@ -1,5 +1,5 @@
 cd ~/'Ubuntu One'/shell
-if [ -z "$1"]; then echo commit remark required;exit; fi
+if [ -z "$1" ]; then echo commit remark required;exit; fi
 git add -i #交互模式
 git commit -am "$1"
 git remote add origin https://github.com/beebon/gitone.git
